162b6211b43dc29fff1a0251835e828e4bfeae08
neroduckale <neroduck@vk.com>
2022-10-28 21:36:09 +0500

    new texture + class for ring

---
+++ src/main/java/com/neroduckale/ringofspeed/ringofthespeedzxc.java
@@ -0,0 +1,10 @@
+package com.neroduckale.ringofspeed;
+
+import net.minecraft.item.Item;
+
+public class ringofthespeedzxc extends Item {
+    public ringofthespeedzxc(Properties properties) {
+
+        super(properties);
+    }
+}
