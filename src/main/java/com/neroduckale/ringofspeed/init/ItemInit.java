ba3f538e2f7f301ad52df21d5fb77fbdcb7ba51f
neroduckale <neroduck@vk.com>
2022-10-28 22:27:15 +0500

    ring class inv ticck

--- src/main/java/com/neroduckale/ringofspeed/init/ItemInit.java
+++ src/main/java/com/neroduckale/ringofspeed/init/ItemInit.java
@@ -9,4 +9,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
     public static final RegistryObject<Item> ring = ITEMS.register("ring", () -> new Item(new Item.Properties()));
+
 }
